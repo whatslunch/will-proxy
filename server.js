40cff08d0fc90d7ebd2993ebae4df50bcc968c53
Will Godfrey <w.gowman.godfrey@gmail.com>
2018-11-26 12:00:54 -0800

    Add functionality for stress testing with new relic

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
+require('newrelic');
 const express = require('express');
-const morgan = require('morgan');
+const axios = require('axios');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 1000;
+const port = process.env.PORT || 9000;
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/:id', (req, res) => {
+  // console.log(req.params);
   res.sendFile(path.join(__dirname, '/public/index.html'));
-})
+});
+
+app.get('/api/overview/:id', (req, res) => {
+  console.log(req.params);
+  axios.get(`http://localhost:9001/api/overview/${ req.params.id }`)
+    .then(response => res.status(200).send(response.data))
+    .catch(err => res.status(500).end(err.message));
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
