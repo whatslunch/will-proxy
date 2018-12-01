12269464dfc7b7bf38d6cff281b2e355126fcab0
Will Godfrey <w.gowman.godfrey@gmail.com>
2018-11-30 18:33:10 -0800

    connect to remote services

--- server.js
+++ server.js
@@ -1,24 +1,49 @@
 require('newrelic');
-const express = require('express');
 const axios = require('axios');
 const path = require('path');
+const proxy = require('http-proxy-middleware');
+
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 9000;
+
+const compress = require('compression');
+app.use(compress());
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/:id', (req, res) => {
-  // console.log(req.params);
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.get('/api/overview/:id', (req, res) => {
-  console.log(req.params);
-  axios.get(`http://localhost:9001/api/overview/${ req.params.id }`)
-    .then(response => res.status(200).send(response.data))
+app.get('/api//reservations', (req, res) => {
+  axios.get('http://34.216.238.63/api/4689cca8-5498-45e3-8761-ab3ce1962358/reservations')
+    .then(resp => res.status(200).send(resp.data))
     .catch(err => res.status(500).end(err.message));
 });
 
+app.use('/api/overview/:id', //will
+  proxy({
+    target: 'http://54.144.31.20',
+    changeOrigin: true
+  })
+);
+
+
+app.use('/api/:id/', //brian
+  proxy({
+    target: 'http://3.16.143.18',
+    changeOrigin: true
+  })
+);
+
+app.use('/restaurants/:id', //mina
+  proxy({
+    target: 'http://18.223.247.137:2000',
+    changeOrigin: true
+  })
+);
+
+const port = process.env.PORT || 9000;
 app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`);
+  console.log(`server running at: http://someplace:${port}`);
 });
